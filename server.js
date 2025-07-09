7bc3f5a1f98e7867ba25fa650be99165b5f01383
Pratik Dubey <Pratikdubey7735@gmail.com>
2025-07-09 17:02:43 +0530

    Describe what you changed

--- server.js
+++ server.js
@@ -4,29 +4,24 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
+const coachRoutes = require("./routes/Coaches");
 
 const app = express();
-app.use(cors({
-  origin:["https://admin-pannel-swart.vercel.app","https://upstep-academy-teaching-platform.vercel.app"]
-}));
+app.use(cors());
 app.use(express.json());
 
+// 🔗 MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.log("MongoDB error:", err));
-app.get("/",(_, res)=>{
-  res.json({
-    success:"Server is running"
-  })
-})
-app.use("/api", authRoutes);
 
+app.use("/api", authRoutes);
+app.use("/api/coaches", coachRoutes);
+
+// ✅ Start Server
 app.listen(process.env.PORT, () =>
   console.log(`Server running on http://localhost:${process.env.PORT}`)
 );
-
-const coachRoutes = require("./routes/Coaches");
-app.use("/api/coaches", coachRoutes);
